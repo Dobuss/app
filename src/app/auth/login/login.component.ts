d8b1d3911fe84893a79193ec8b196a575002d27f
Dobuss <dobromira.ivanova911@gmail.com>
2022-11-22 18:18:18 +0200

    Implementing additional views

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  constructor(private router: Router, private AuthService: AuthService) {
+      /*this.AuthService.user = {
+        username: 'John'
+      } as any;
+      this.router.navigate(['/']);*/
+   }
+}
